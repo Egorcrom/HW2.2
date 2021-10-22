da538a3f8f5ef6bd1b90b1533e00aac1810ead22
Egor Kromin <egorcrom@mail.ru>
2021-10-22 19:38:51 +0300

    back-end added

--- HW2.2/ViewController.swift
+++ HW2.2/ViewController.swift
@@ -8,12 +8,50 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var redSliderOutlet: UISlider!
+    @IBOutlet var greenSliderOutlet: UISlider!
+    @IBOutlet var blueSliderOutlet: UISlider!
+    
+    @IBOutlet var redLabelValue: UILabel!
+    @IBOutlet var greenLabelValue: UILabel!
+    @IBOutlet var blueLabelValue: UILabel!
+    
+    @IBOutlet var coloredView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        coloredView.layer.cornerRadius = 30
+        coloredView.layer.borderWidth = 1
+        coloredView.layer.borderColor = UIColor.black.cgColor
+        
+        redSliderOutlet.minimumValue = 0
+        greenSliderOutlet.minimumValue = 0
+        blueSliderOutlet.minimumValue = 0
+        redSliderOutlet.maximumValue = 1
+        greenSliderOutlet.maximumValue = 1
+        blueSliderOutlet.maximumValue = 1
+        redSliderOutlet.value = 0.56
+        greenSliderOutlet.value = 0.56
+        blueSliderOutlet.value = 0.58
+        
+        redLabelValue.text = String(redSliderOutlet.value)
+        greenLabelValue.text = String(greenSliderOutlet.value)
+        blueLabelValue.text = String(blueSliderOutlet.value)
+
     }
 
-
+    
+    
+    @IBAction func sliderAction() {
+        redLabelValue.text = String(format: "%.2f", redSliderOutlet.value)
+        greenLabelValue.text = String(format: "%.2f", greenSliderOutlet.value)
+        blueLabelValue.text = String(format: "%.2f", blueSliderOutlet.value)
+        
+        coloredView.backgroundColor = UIColor(red: CGFloat(redSliderOutlet.value), green: CGFloat(greenSliderOutlet.value), blue: CGFloat(blueSliderOutlet.value), alpha: 1)
+    }
+    
+        
+    
 }
 
